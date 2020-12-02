0db14aa245ad7bd9e6c895b0965e21fd33a62c71
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-12-02 16:40:36 +0000

    solve second exercise part 1

--- problems.clj
+++ problems.clj
@@ -1,10 +1,11 @@
-(ns problems)
+(ns problems
+  (:require [clojure.string :as str]))
 
 (defn read-input
   [n]
   (-> (format "%s.txt" n)
       slurp
-      clojure.string/split-lines))
+      str/split-lines))
 
 (def p1 (map #(Integer/parseInt %) (read-input 1)))
 
@@ -20,3 +21,20 @@
           c p1]
     (when (= 2020 (+ a b c))
       (println a, b, c (* a b c)))))
+
+(def p2 (read-input 2))
+
+(defn valid-line? [l]
+  (let [[from to ch password]
+        (->> l
+             (re-find #"((\d+)-(\d+)) (\w): (\w+)")
+             (drop 2)
+             vec)
+        occ (get (frequencies password)
+                 (first ch))]
+
+    (and (some? occ)
+         (<= (Integer/parseInt from) occ (Integer/parseInt to)))))
+
+(count
+ (filter valid-line? p2));; => 515
